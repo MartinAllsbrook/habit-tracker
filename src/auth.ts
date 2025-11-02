c81eb56c1d9a8a729aae402b7b34c14cf6465991
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2025-11-02 12:48:09 -0800

    Add Prisma adapter to store sessions in DB

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,11 @@ import NextAuth from "next-auth"
 import type { NextAuthConfig } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import process from "node:process"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/prisma.ts"
 
 export const authConfig: NextAuthConfig = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHub({
             clientId: process.env.GITHUB_ID!,
