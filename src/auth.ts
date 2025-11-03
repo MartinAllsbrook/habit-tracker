4fbe74560d544b8fa0409f715b88fe1b3a6355b1
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2025-11-02 16:42:48 -0800

    Fix prisma import

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import type { NextAuthConfig } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import process from "node:process"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "@/lib/prisma"
+import { prisma } from "@/lib/prisma"
 
 export const authConfig: NextAuthConfig = {
     adapter: PrismaAdapter(prisma),
